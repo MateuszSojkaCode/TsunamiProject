ed1c111414ce5b38c7db96c12125c0f850df96aa
Mateusz <matsoj01@gmail.com>
2023-01-28 15:52:56 +0000

    footer is finished, mobile and desktop, add script for checking current year

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,13 @@
 const burgerBtn = document.querySelector('.nav__burgerBtn')
 const closeBtn = document.querySelector('.nav__close')
 const mobileNav = document.querySelector('.nav__list-box')
+const year = document.querySelector('.year')
 
 const showNav = () => {
     mobileNav.classList.toggle('hide')
 }
+
+year.innerHTML = new Date().getFullYear();
 
 burgerBtn.addEventListener('click', showNav)
 closeBtn.addEventListener('click', showNav)
