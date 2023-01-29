ba35ffb8dce1f3a563103ac925036f732edfcf65
Mateusz <matsoj01@gmail.com>
2023-01-29 13:06:48 +0000

    FAQ section is finished

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,17 @@ const burgerBtn = document.querySelector('.nav__burgerBtn')
 const closeBtn = document.querySelector('.nav__close')
 const mobileNav = document.querySelector('.nav__list-box')
 const year = document.querySelector('.year')
+const toggles = document.querySelectorAll('.faq__toggle')
 
 const showNav = () => {
     mobileNav.classList.toggle('hide')
 }
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('faq__active')
+    })
+})
 
 year.innerHTML = new Date().getFullYear();
 
